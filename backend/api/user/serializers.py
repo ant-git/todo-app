ce4aba4e4163529d90a04f6d247940d62ee61068
Anton <antonkhvdv@gmail.com>
2020-01-21 09:05:49 +0300

    Added manage user endpoint and tests

--- backend/api/user/serializers.py
+++ backend/api/user/serializers.py
@@ -1,11 +1,10 @@
 from django.contrib.auth import get_user_model, authenticate
 from django.utils.translation import ugettext_lazy as _
-
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    """Serializer for users object"""
+    """"Serializer for the user object"""
 
     class Meta:
         model = get_user_model()
@@ -15,6 +14,15 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
@@ -36,7 +44,7 @@ class AuthTokenSerializer(serializers.Serializer):
             password=password
         )
         if not user:
-            msg = ('Unable to authenticate with provided credentials')
+            msg = _('Unable to authenticate with provided credentials')
             raise serializers.ValidationError(msg, code='authentication')
 
         attrs['user'] = user
